900ed3fbe97fe045c3926e98a2ccbd2724620f7f
gitscott <scott@fabergroup.co>
2022-10-02 20:04:37 -0400

    Added sub dependency

--- tests/crap_test.py
+++ tests/crap_test.py
@@ -1,5 +1,7 @@
 from crap_utility.crap_logger import crap_logger
 from crap_utility.crap_job import job1, job2
+from crap_utility.crap_pandas import crap_frame
+import pandas as pd
 
 import unittest
 
@@ -9,9 +11,13 @@ class TestStringMethods(unittest.TestCase):
         logger.info('This is: %s', __name__)
         self.assertEqual(job1(), None)
     
-    def test_job1(self):
+    def test_job2(self):
         logger.info('This is: %s', __name__)
         self.assertEqual(job2(), None)
+
+    def test_crap_frame(self):
+        logger.info('Testing data frame creation')
+        self.assertEqual(type(crap_frame()), pd.Series)
 
 logger = crap_logger()
 
